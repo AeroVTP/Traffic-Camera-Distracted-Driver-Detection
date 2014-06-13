8cedd710fb44a4584997247c8f82d5ed35235f76
AeroVTP <aerovtp@gmail.com>
2014-06-13 08:57:05 -0400

    Creating initial start and end of function.

---
+++ trafficCameraDistractedDriverDetectionGit.m
@@ -0,0 +1,7 @@
+function [] = trafficCameraDistractedDriverDetection(backgroundFrame, framesToProcess)
+
+	%returning back a string to make this appear to Octave as a function
+	finished = 'done';
+	
+%this will never finish when this is turned into a live stream. 
+end
