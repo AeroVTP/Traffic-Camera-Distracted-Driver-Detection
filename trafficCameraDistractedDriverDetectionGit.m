45a682ca1d993ecfc7fc26664cb3223aff8c865d
AeroVTP <aerovtp@gmail.com>
2014-06-13 09:53:04 -0400

    Added initial image blurring

--- trafficCameraDistractedDriverDetectionGit.m
+++ trafficCameraDistractedDriverDetectionGit.m
@@ -1,5 +1,16 @@
 function [] = trafficCameraDistractedDriverDetection(backgroundFrame, framesToProcess)
+	
+	%converting B to grayscale format before saving 
+	B = rgb2gray(B);
+	
+	imwrite(B, 'afterconv2.PNG');
+	
+	%creating a Gaussian filter to blur the image	
+	f_gauss = fspecial("gaussian", 1, 10);
+	
+	%applying the Gaussian filter to blur the image
 
+	B = filter2(f_gauss, B, "valid");
 	%returning back a string to make this appear to Octave as a function
 	finished = 'done';
 	
